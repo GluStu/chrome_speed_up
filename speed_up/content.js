5910644006e58096764939fdd11fc952b6401665
GluStu <114978785+GluStu@users.noreply.github.com>
2025-09-22 15:44:02 +0530

    Update content.js

--- speed_up/content.js
+++ speed_up/content.js
@@ -1,8 +1,8 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.speed !== undefined) {
-      const mediaElements = document.querySelectorAll('video, audio');
-      mediaElements.forEach(element => {
-        element.playbackRate = request.speed;
-      });
-    }
-  });  
+  if (request.speed !== undefined) {
+    const mediaElements = document.querySelectorAll('video, audio');
+    mediaElements.forEach(element => {
+      element.playbackRate = request.speed;
+    });
+  }
+});
